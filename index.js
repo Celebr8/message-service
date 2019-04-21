41ccf280ff0e66a4693ca4b4d6cc229a8ebcfd82
Loic Coenen <ijwtcymb@gmail.com>
2019-04-21 16:19:33 +0100

    Add reCaptcha validation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const restify = require('restify');
 const dotenv = require('dotenv');
 const sendgrid = require('@sendgrid/mail');
 const corsMiddleware = require('restify-cors-middleware');
+const Recaptcha = require('recaptcha-verify');
 
 // Environement
 
@@ -15,9 +16,11 @@ const dev = process.env.NODE_ENV != 'production';
 
 const sendgridApiKey = process.env.SENDGRID_API_KEY;
 const serviceDestinationEmail = process.env.SERVICE_DESTINATION_EMAIL;
+const recaptchaSecretKey = process.env.RECAPTCHA_SECRET_KEY;
 
 if (!sendgridApiKey) throw Error('No Sendgrid API key defined');
 if (!serviceDestinationEmail) throw Error('No service destination email');
+if (!recaptchaSecretKey) throw Error('No reCaptcha secret key');
 
 // Initialisation
 
@@ -25,7 +28,14 @@ const server = restify.createServer();
 
 sendgrid.setApiKey(sendgridApiKey);
 
+if (dev) console.log('Development mode');
+
 const origins = dev ? ['*'] : ['http://www.whichost.com'];
+
+const recaptcha = new Recaptcha({
+  secret: recaptchaSecretKey,
+  verbose: dev,
+});
 
 const cors = corsMiddleware({
   preflightMaxAge: 5, //Optional
@@ -43,7 +53,21 @@ server.use(
 server.pre(cors.preflight);
 server.use(cors.actual);
 
-server.post('/message', (req, res, next) => {
+async function validateRecaptcha(token) {
+  return new Promise((resolve, reject) => {
+    // reCaptcha validation
+
+		recaptcha.checkResponse(token, (error, response) => {
+
+			console.log('Recieved a response: ', response)
+
+			if(error) reject(new Error(error));
+			else resolve(response);	
+		});
+  });
+}
+
+server.post('/message', async (req, res, next) => {
   const params = JSON.parse(req.body);
 
   // Validation
@@ -53,7 +77,7 @@ server.post('/message', (req, res, next) => {
       (soFar, key) => soFar && Object.keys(params).indexOf(key) != -1,
     );
 
-  if (!checkParams(params, ['email', 'phoneNumber', 'message', 'subject'])) {
+  if (!checkParams(params, ['email', 'phoneNumber', 'message', 'subject', 'recaptchaToken'])) {
     if (!params.email)
       res.send(400, {status: 400, message: 'No email defined'});
     else if (!params.phoneNumber)
@@ -62,9 +86,23 @@ server.post('/message', (req, res, next) => {
       res.send(400, {status: 400, message: 'No message defined'});
     else if (!params.subject)
       res.send(400, {status: 400, message: 'No subject defined'});
+    else if (!params.recaptchaToken)
+      res.send(400, {status: 400, message: 'No recaptcha Token defined'});
 
     return next();
   }
+
+	// reCaptcha validation
+
+	try {
+		const recaptchaResponse = await validateRecaptcha(params.recaptchaToken);
+		if(!recaptchaResponse.success) throw Error('You are a robot', recaptchaResponse)
+	}
+	catch(error) {
+		console.log('reCaptcha validation failed', error)
+		res.send(400, {status: 400, message: 'reCaptcha validation failed', error})	
+		return next();
+	}
 
   // Sending
 
@@ -93,23 +131,19 @@ server.post('/message', (req, res, next) => {
   sendgrid
     .send(data)
     .then(() => {
-
       console.log('Send email');
       res.send(JSON.stringify({status: 200, message: 'Send email'}));
       next();
-
     })
     .catch(error => {
-
       console.log('Fail to send email.');
       console.log(error);
 
-      res.send(
+      res.send(500, 
         JSON.stringify({status: 500, message: 'Failed to send email', error}),
       );
 
       next();
-
     });
 });
 
